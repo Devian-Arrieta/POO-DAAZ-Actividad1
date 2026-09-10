0212341260177eb58b86e52c71f9fd2f6e0ece55
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-09-10 12:19:37 -0500

    10/09/2026 - Ejercicio 16 correspondiente a la parte C hecho correctamente

---
+++ src/Parte_C/Ejercicio16/Producto.java
@@ -0,0 +1,76 @@
+package Parte_C.Ejercicio16;
+
+public class Producto {
+
+    String nombre;
+    double precio;
+    int stock;
+
+    public void mostrarProducto(){
+        System.out.println(
+            "INFORMACIÓN DEL PRODUCTO \n" +
+            "Nombre: "+ nombre +"\n"+
+            "Precio: "+ precio +"\n"+
+            "Stock: "+ stock +"\n"
+        );
+    }
+
+    public void vender(int cantidad){
+
+        if (cantidad > stock){
+            System.out.println("La cantidad de productos a vender es mayor a lo disponible en el stock");
+        }
+        else{
+            stock -= cantidad;
+            System.out.println("Producto vendido correctamente");
+        }
+    }
+
+    public void reabastecer(int cantidad){
+        stock += cantidad;
+    }
+}
+
+class Main{
+    public static void main(String[] args) {
+
+        Producto producto1 = new Producto();
+        producto1.nombre = "Pan tajado";
+        producto1.precio = 8000;
+        producto1.stock = 20;
+
+        Producto producto2 = new Producto();
+        producto2.nombre = "Cartón de huevos";
+        producto2.precio = 12000;
+        producto2.stock = 60;
+
+        Producto producto3 = new Producto();
+        producto3.nombre = "Bolsa de tomates";
+        producto3.precio = 5000;
+        producto3.stock = 30;
+
+        System.out.print("PRODUCTO 1: ");
+        producto1.mostrarProducto();
+
+        System.out.print("PRODUCTO 2: ");
+        producto2.mostrarProducto();
+
+        System.out.print("PRODUCTO 3: ");
+        producto3.mostrarProducto();
+
+        // productos vendidos
+
+        producto1.reabastecer(70);
+        System.out.print("PRODUCTO 1 REABASTECIDO: ");
+        producto1.mostrarProducto();
+
+        producto2.reabastecer(100);
+        System.out.print("PRODUCTO 2 REABASTECIDO: ");
+        producto2.mostrarProducto();
+
+        producto2.reabastecer(45);
+        System.out.print("PRODUCTO 3 REABASTECIDO: ");
+        producto3.mostrarProducto();
+
+    }
+}
