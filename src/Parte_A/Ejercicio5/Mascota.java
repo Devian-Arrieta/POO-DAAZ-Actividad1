2d6b654ad3674a337f85c53fb0b408a074bf4e50
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-09-09 20:47:50 -0500

    9/09/2026 - Ejercicio 5 de la parte A hecho correctamente

---
+++ src/Parte_A/Ejercicio5/Mascota.java
@@ -0,0 +1,16 @@
+package Parte_A.Ejercicio5;
+
+public class Mascota {
+
+    String nombre, especie;
+    int edad;
+
+    public void mostrarMascota(){
+        System.out.println(
+            "INFORMACIÓN DE LA MASCOTA \n \n" +
+            "Nombre: "+ nombre +"\n"+
+            "Especie: "+ especie +"\n"+
+            "Edad: "+ edad +"\n"
+        );
+    }
+}
