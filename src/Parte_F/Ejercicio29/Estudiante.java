0baa24bf4571fbd07b7592f7248ffbb8d3f62e13
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-09-10 20:22:03 -0500

    10/09/2026 - Ejercicio 29 correspondiente a la parte F hecho correctamente

---
+++ src/Parte_F/Ejercicio29/Estudiante.java
@@ -0,0 +1,40 @@
+package Parte_F.Ejercicio29;
+
+public class Estudiante {
+
+    public String nombre, codigo, semestre;
+
+    public Estudiante(){
+
+    }
+    
+    public Estudiante(String nombre, String codigo){
+        this.nombre = nombre;
+        this.codigo = codigo;
+    }
+
+    public Estudiante(String nombre, String codigo, String semestre){
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.semestre = semestre;
+    }
+
+    public void mostrarInfo(){
+        System.out.println(
+            "INFORMACIÓN DEL ESTUDIANTE \n" +
+            "Nombre: "+ nombre +"\n"+
+            "Código: "+ codigo +"\n"+
+            "Semestre: "+ semestre +"\n"
+        );
+    }
+
+    public void cambiarSemestre(String nuevoSemestre){
+        semestre = nuevoSemestre;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+    }
+}
