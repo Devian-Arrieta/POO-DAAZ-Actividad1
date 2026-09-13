26ddb0711c888bb1549ba88c2086b3e0f4998fc8
Devian Arrieta <darrietaz@unicartagena.edu.co>
2026-09-12 19:39:35 -0500

    12/09/2026 - Ejercicio 54 correspondiente a la parte J hecho correctamente

---
+++ src/Parte_J/Ejercicio54/Semaforo.java
@@ -0,0 +1,40 @@
+package Parte_J.Ejercicio54;
+
+public class Semaforo {
+
+    String colorActual;
+
+    public Semaforo(String colorInicial) {
+        this.colorActual = colorInicial;
+    }
+
+    public void cambiarColor(String nuevoColor) {
+        if (nuevoColor.equalsIgnoreCase("Rojo") ||
+                nuevoColor.equalsIgnoreCase("Amarillo") ||
+                nuevoColor.equalsIgnoreCase("Verde")) {
+
+            this.colorActual = nuevoColor;
+        }
+        else { // valida que el cambio de color del semaforo sean los colores correctos
+            System.out.println("Error: '" + nuevoColor + "' no es un color válido");
+        }
+    }
+
+    public void mostrarEstado() {
+        System.out.println("Semáforo en: " + colorActual);
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+
+        Semaforo semaforo = new Semaforo("Rojo");
+        semaforo.mostrarEstado();
+
+        semaforo.cambiarColor("Verde");
+        semaforo.mostrarEstado();
+        
+        semaforo.cambiarColor("Azul");
+        semaforo.mostrarEstado();
+    }
+}
